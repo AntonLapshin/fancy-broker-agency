2f79fcbddb07e664f646085e29508814f4b0b669
Anton Lapshin <antvallap@gmail.com>
2018-08-11 12:16:45 +0200

    contact page plug

--- src/routes/contacts/contact/Contact.js
+++ src/routes/contacts/contact/Contact.js
@@ -1,8 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default () => (
-    <div className="page">
-      <h2>Contact</h2>
-    </div>
-  );
-  
+const Contact = props => (
+  <div className="page">
+    <h2>Contact {props.match.params.id}</h2>
+    <p>Show contact details here</p>
+  </div>
+);
+
+Contact.propTypes = {
+  match: PropTypes.object.isRequired
+};
+
+export default Contact;
